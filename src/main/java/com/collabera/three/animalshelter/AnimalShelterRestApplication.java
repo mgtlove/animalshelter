b5deda7916f8f17a2f56759188f0186b1a22251b
Matthew <mgtlove@gmail.com>
2020-07-24 17:26:16 -0500

    changes for heroku

--- src/main/java/com/collabera/three/animalshelter/AnimalShelterRestApplication.java
+++ src/main/java/com/collabera/three/animalshelter/AnimalShelterRestApplication.java
@@ -2,9 +2,10 @@ package com.collabera.three.animalshelter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnimalShelterRestApplication
+public class AnimalShelterRestApplication extends  SpringBootServletInitializer
 {
 
 	public static void main(String[] args)
